5a43773426ef6148c3f8235d21036166772eaddb
Aarav raj <anshuk3917@gmail.com>
2023-07-07 19:34:41 +0530

    Updated landing

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Find, Sell, Buy a car—quick and super easy!
         </h1>
 
         <p className="hero__subtitle">
