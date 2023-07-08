29afe353028e3b144645106961bd8c7e80ea5a90
Aarav raj <anshuk3917@gmail.com>
2023-07-08 08:41:54 +0530

    Finally minting nft's

--- components/MIntCar.js
+++ components/MIntCar.js
@@ -1,39 +1,45 @@
 "use client"
 import React, { useState } from 'react';
-import mintCarNFT from "../utils/contract"
+import { ethers } from 'ethers';
+import abi from '../app/abi.json'
+
+const contractAddress = "0xC9ab74cc4927086F2672b95015577f49dAEBa847";
+
 function MintCar() {
-    const [VIN, setVIN] = useState("");
-    const [model, setModel] = useState("");
-    const [make, setMake] = useState("");
-    const [mileage, setMileage] = useState("");
-    const [maintenanceHistory, setMaintenanceHistory] = useState("");
-    const [price, setPrice] = useState("");
+  const [VIN, setVIN] = useState("");
+  const [model, setModel] = useState("");
+  const [make, setMake] = useState("");
+  const [mileage, setMileage] = useState("");
+  const [maintenanceHistory, setMaintenanceHistory] = useState("");
+  const [price, setPrice] = useState("");
 
-    const handleSubmit = async () => {
-        const data = [VIN, model, make, mileage, maintenanceHistory, price];
-        try {
-            // Call your mintCarNFT function here
-            await mintCarNFT(data);
-            alert('Success');
-        } catch (err) {
-            console.error(err);
-            alert('Failed');
-        }
-    };
+  async function mintCarNFT() {
+      await window.ethereum.enable();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
 
-    return (
-        <div className="m-4">
-          <div className="flex flex-col space-y-2">
-            <input type="text" placeholder="VIN" value={VIN} onChange={(e) => setVIN(e.target.value)} className="border p-2" />
-            <input type="text" placeholder="Model" value={model} onChange={(e) => setModel(e.target.value)} className="border p-2" />
-            <input type="text" placeholder="Make" value={make} onChange={(e) => setMake(e.target.value)} className="border p-2" />
-            <input type="text" placeholder="Mileage" value={mileage} onChange={(e) => setMileage(e.target.value)} className="border p-2" />
-            <input type="text" placeholder="Maintenance History" value={maintenanceHistory} onChange={(e) => setMaintenanceHistory(e.target.value)} className="border p-2" />
-            <input type="text" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} className="border p-2" />
-          </div>
-          <button onClick={handleSubmit} className="bg-blue-500 text-white p-2 rounded-md mt-2">Mint</button>
-        </div>
-      );
-    } 
+      try {
+          const tx = await contract.mintCarNFT(VIN, model, make, mileage, maintenanceHistory, ethers.utils.parseEther(price));
+          console.log('Transaction sent');
+          await tx.wait();
+          console.log('Transaction mined');
+      } catch (err) {
+          console.error('Failed to mint NFT', err);
+      }
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '25px', padding: '20px', width: '1000px' }}>
+    <input placeholder="VIN" onChange={e => setVIN(e.target.value)} />
+    <input placeholder="Model" onChange={e => setModel(e.target.value)} />
+    <input placeholder="Make" onChange={e => setMake(e.target.value)} />
+    <input placeholder="Mileage" onChange={e => setMileage(e.target.value)} />
+    <input placeholder="Maintenance History" onChange={e => setMaintenanceHistory(e.target.value)} />
+    <input placeholder="Price" onChange={e => setPrice(e.target.value)} />
+    <button onClick={mintCarNFT} style={{ backgroundColor: 'blue', color: 'white' }}>Mint NFT</button>
+</div>
+  );
+}
 
 export default MintCar;
